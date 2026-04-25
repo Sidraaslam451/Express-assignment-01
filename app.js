58be1bbe1ff228c288ccfa98f19a258eee2f37db
sidra aslam <aslamsidra876@gmail.com>
2026-04-25 18:14:10 +0500

    c

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 let users = [];
 app.post("/api/users", (req, res) => {
   const user = req.body;
